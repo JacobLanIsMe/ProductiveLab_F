15b118b53bf3abee7cd30dd09ceae1a655572bb4
藍紹瑋 <jacob.lan@cloudnifier.com>
2023-03-10 23:49:14 +0800

    加入了 headder 和 Main Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './index/header/header.component';
+import { MainPageComponent } from './index/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    IndexComponent,
+    HeaderComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
