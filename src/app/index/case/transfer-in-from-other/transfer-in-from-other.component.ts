fa8674b9358aed3ea2d0d0c9744d25514d2ebbda
藍紹瑋 <jacob.lan@cloudnifier.com>
2023-05-15 23:27:45 +0800

    transferInFromOutside start

--- src/app/index/case/transfer-in-from-other/transfer-in-from-other.component.ts
+++ src/app/index/case/transfer-in-from-other/transfer-in-from-other.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DateService } from 'src/app/@Service/date.service';
 
 @Component({
   selector: 'app-transfer-in-from-other',
   templateUrl: './transfer-in-from-other.component.html',
   styleUrls: ['./transfer-in-from-other.component.css']
 })
-export class TransferInFromOtherComponent {
+export class TransferInFromOtherComponent implements OnInit {
+  constructor(private dateService:DateService){}
+  ngOnInit(): void {
+    this.transferInForm = new FormGroup({
+      "transferInTime": new FormControl(this.dateService.getTodayDateTimeString,Validators.required),
+      ""
+    })
+  }
 
+  transferInForm!:FormGroup;
 }
